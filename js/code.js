d21e641bcce28c44d6a31fb9682a1c415ef9955a
Nathan Hicks <142943595+WafflerIron@users.noreply.github.com>
2024-09-08 16:43:26 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -139,40 +139,6 @@ function addContact()
 	
 }
 
-function doSignup()
-{
-	userId = 0;
-	
-	let firstName = document.getElementById("firstname").value;
-	let lastName = document.getElementById("lastname").value;
-	let login = document.getElementById("username").value;
-	let password = document.getElementById("password").value;
-
-	let tmp = {firstName:firstName, lastName:lastName, login:login,password:password};
-	let jsonPayload = JSON.stringify( tmp );
-
-	let url = urlBase + '/AddUsers.' + extension;
-	
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.onreadystatechange = function() 
-		{
-			if (this.readyState == 4 && this.status == 200) 
-			{
-				window.location.href = "color.html";
-			}
-		};
-		xhr.send(jsonPayload);
-	}
-	catch(err)
-	{
-		//document.getElementById("colorAddResult").innerHTML = err.message;
-	}
-	
-}
 
 function searchContact()
 {
