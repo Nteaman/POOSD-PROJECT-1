be42f923dbb9590e11d1f48a27f5c003e790559c
Thomas Kern <142808316+Thomas1034@users.noreply.github.com>
2024-09-08 16:53:38 -0400

    Corrected API URL

--- js/code.js
+++ js/code.js
@@ -1,5 +1,8 @@
 const urlBase = 'http://nteaman_instance1.natalieteaman.site';
+const api = '/LAMPAPI';
 const extension = 'php';
+// Set to "" later
+const testBranch = '/test/POOSD-PROJECT-1';
 
 let userId = 0;
 let firstName = "";
