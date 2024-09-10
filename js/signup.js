5f8ce0e4ae3ba4536dc81621c1fd08205d9f56c6
Nathan Hicks <142943595+WafflerIron@users.noreply.github.com>
2024-09-10 11:20:37 -0400

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -2,16 +2,11 @@
 function doSignup() {
 	userId = 0;
 	let flag = true;
-	
-	//document.getElementById("createResult").innerHTML = "No fields blank pleaseee";
-
-	
-	
 		
-		let firstName = document.getElementById("firstname").value;
-		let lastName = document.getElementById("lastname").value;
-		let login = document.getElementById("username").value;
-		let password = document.getElementById("password").value;
+	let firstName = document.getElementById("firstname").value;
+	let lastName = document.getElementById("lastname").value;
+	let login = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
 	
 	
 
@@ -26,14 +21,12 @@ function doSignup() {
 	try {
 		xhr.onreadystatechange = function () {
 			if (this.readyState == 4 && this.status == 200) {
-				//document.getElementById("createResult").innerHTML = "success!";
 				window.location.href = "Landingpage.html";
 			}
 		};
 		xhr.send(jsonPayload);
 	}
 	catch (err) {
-		//document.getElementById("colorAddResult").innerHTML = err.message;
 	}
 
 }
