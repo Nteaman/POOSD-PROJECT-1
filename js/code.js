172ddd24863f6092a65fdf13a25887177142a84d
Nathan Hicks <142943595+WafflerIron@users.noreply.github.com>
2024-09-16 21:46:57 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -269,10 +269,11 @@ function editContact(row) {
   	// Append a text node to the cell
   	newText = document.createElement('button');
 	newText.class="edit-button";
-	newText.onClick = alert(id);
 	newText.id = "confirmButton";
 	newText.innerHTML = "Confirm!";
 	newCell.appendChild(newText);
+
+	document.getElementById('confirmButton').onClick = alert(id);
 
 
 }
