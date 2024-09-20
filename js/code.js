0e48dcc1cab200a06014465b371ef86665eb2ecb
da804107 <daisanne42@gmail.com>
2024-09-20 18:15:06 -0400

    adding toast

--- js/code.js
+++ js/code.js
@@ -370,6 +370,7 @@ function goDashboard(){
 
 function showToast() {
     const toast = document.getElementById('toast');
+	toast.textContent = message;
     toast.classList.add('show');
     setTimeout(() => {
         toast.classList.remove('show');
