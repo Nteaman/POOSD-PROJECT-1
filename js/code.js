9f85ee157c59c9604e31baaf2609d5a44f49cbf9
Nathan Hicks <142943595+WafflerIron@users.noreply.github.com>
2024-09-20 21:07:55 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -42,6 +42,7 @@ function doLogin()
 				if( userId < 1 )
 				{		
 					//document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+					showToast("User/Password combination incorrect");
 					return;
 				}
 		
